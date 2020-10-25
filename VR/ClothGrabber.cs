acab23841c35f8487e6acd0dd57e613925b1d7cb
president saitama-tec inc <saitama-president@users.noreply.github.com>
2020-10-25 18:26:49 +0900

    huhuh
    hhgh

---
+++ VR/ClothGrabber.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SkinmeshRenderer))]
+public class ClothGrabber : MonoBehaviour
+{
+//掴みアクションが発生した場合にラグドールにスキンメッシュを移管する
+
+  
+  void Update()
+  {
+    
+  }
+}
