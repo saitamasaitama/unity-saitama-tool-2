7cc18e9e0ca6fcf4997b215c5a601495dcc47bf9
yoshihara shinpei <yoshihara-shinpei@av.avex.co.jp>
2020-10-31 01:22:39 +0900

    aaaa

--- RigidBody/phisicRingCreator.cs
+++ RigidBody/phisicRingCreator.cs
@@ -16,36 +16,54 @@ public class phisicRingCreator : MonoBehaviour
   public void Create()
   {
 
-    GameObject o = new GameObject("PhysicRing");
 
+
+    //CreateRing();
+
+    CreateSphere();
+  }
+
+
+  private void CreateSphere()
+  {
+    //円状に
+    GameObject o = new GameObject("PhysicSphere");
+
+
+
+  }
+
+  private void CreateRing()
+  {
+    GameObject o = new GameObject("PhysicRing");
     RingList<SpringJoint> joints = new RingList<SpringJoint>();
 
     for (int i = 0; i < count; i++)
     {
       float round = (float)(360.0 / count * i);
-      var bit=new GameObject($"Ring{i}");
-      var sphere=GameObject.CreatePrimitive(PrimitiveType.Sphere);
+      var bit = new GameObject($"Ring{i}");
+      var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
       sphere.transform.localScale = Vector3.one * radius / count * Mathf.PI;
       sphere.transform.SetParent(bit.transform);
       bit.transform.SetParent(o.transform);
-      bit.transform.localPosition = Quaternion.Euler(0,round,0)*Vector3.forward * radius / 2.0f ;
+      bit.transform.localPosition = Quaternion.Euler(0, round, 0) * Vector3.forward * radius / 2.0f;
 
 
       var rigid = bit.AddComponent<Rigidbody>();
-      var spring= bit.AddComponent<SpringJoint>();
+      var spring = bit.AddComponent<SpringJoint>();
 
 
       joints += spring;
-      
+
     }
 
     //連結していく
-    foreach(var item in joints)
+    foreach (var item in joints)
     {
       Debug.Log($"Joint+[ {item.Value.name}  <==> {item.Next.Value.name} ]");
       item.Value.connectedBody = item.Next.Value.gameObject.GetComponent<Rigidbody>();
     }
 
-
   }
+
 }
