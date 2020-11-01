e5d59694ce2e187270cd6196433fe0182b849c0f
shinp <shinpeiyoshihara@gmail.com>
2020-11-02 02:40:41 +0900

    aaaa

---
+++ Canvas/LabelUpdater.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class LabelUpdater : MonoBehaviour
+{
+  public Text Text => GetComponentInChildren<Text>();
+  public string StartText = "";
+
+  public string FloatFormat = "0.00";
+  public string DateTimeFormat = "yyyy-MM-dd(ddd) HH:mm";
+  // Start is called before the first frame update
+  void Start()
+  {
+    Text.text = StartText;
+  }
+
+  public void OnChangeByFloat(float f)
+  {
+    Text.text = f.ToString(FloatFormat);
+  //  s.onValueChanged
+  }
+
+  public void OnChangeByDateTime(DateTime d)
+  {
+    Text.text = d.ToString(DateTimeFormat);
+  }
+
+
+}
---
+++ Time/SunControll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SunControll : MonoBehaviour
+{
+  private float r = 0;
+
+  void Update()
+  {
+    TimeSpan span = GameTime.Now.ToUniversalTime() 
+      - new DateTime(1970, 1, 1,21,15,0);
+    //とりあえず1日=360度とする
+    float dayTime = (float)(span.TotalSeconds  % 86400.0);
+    float x = dayTime / 86400.0f  * 360.0f;
+
+    this.transform.localRotation =Quaternion.Euler(x, 0, 0);
+
+
+
+
+  }
+}
