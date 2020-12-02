d21a764c4419e5e2347fc1c21dc211a3c80b5f40
shinpei_yoshihara <s-yoshihara@saitama-tec.co.jp>
2020-12-02 15:30:09 +0900

    narrative world add

---
+++ NarrativeWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NarrativeWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
